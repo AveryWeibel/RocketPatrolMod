5b201c3aa3bdce397daaa5d2e18905b6fe84f41d
mrzebrasmoothie <averyweibel@yahoo.com>
2021-04-12 12:27:05 -0700

    Added ship explosions (and the reset of the game we did in class)

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,45 @@
+  
+// Rocket (player) prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to the existing scene
+        scene.add.existing(this);
+        this.isFiring = false;      // track rocket firing status
+        this.moveSpeed = 2;         // pixels per frame
+    }
+
+    update(time, delta) {
+        let deltaMultiplier = (delta/16.66667);
+
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed * deltaMultiplier;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed * deltaMultiplier;
+            }
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+
+        }
+
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed * deltaMultiplier;
+
+        }
+
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}
---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,24 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this); //Add to existing scene
+        this.points = pointValue; //Store instance pointvalue
+        this.moveSpeed = 3;       //Gaming
+    }
+
+    update (time, delta) {
+        let deltaMultiplier = (delta/16.66667);
+
+        // Move spaceship
+        this.x -= this.moveSpeed * deltaMultiplier;
+        //Wrap around from left to right edge
+        if(this.x  <= 0 - this.width) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.x = game.config.width;
+
+    }
+}
