c9cdaf1b33d0ea69ba239043fa5c93beb0619d0c
mrzebrasmoothie <averyweibel@yahoo.com>
2021-04-21 11:53:36 -0700

    Submission comment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-  
+
+/***********************/
+//Avery Weibel
+//Rocket Patrol Mod
+//4/21/21
+
+//FULL MOD TIME: 13.25hrs
+
+//Initial polar coordinate movement - 3 hrs
+//Firing and reset - 1hr
+//Comments/freshening variables .5hrs
+//Give enemies polar movement .5hrs
+//Enemies polar resetting .5hrs
+//Player animations 1.5hrs
+//Enemy animations .5hrs
+//Env art 3hrs
+//Menu art 1.5hrs
+//new text 1hrs
+//Timer functionality 1.5hrs\
+//New sounds - .75hrs
+//New music - 1hr
+
+//Score: Polar gameplay 30
+//       Art overhaul (Molecule Combination/Mars Base Theme) 60
+//       Timer on screen 10
+//       Timer updates on hit 20
+//       Enemies randomized direction 5
+//       New animated sprite for enemies 10 (The art overhaul doesn't say animated sprite so seems like thats added)
+
+/********************** */
+
 // game configuration
 let config = {
     type: Phaser.CANVAS,
